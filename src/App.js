7474985bed312cbff7edabfd01db1f056537d15a
Oluwatosin <hezealice@gmail.com>
2022-11-29 17:11:22 +0100

    navigation bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 // import AllProducts from './components/AllProducts/AllProducts';
-import React from "react";
-import Navbar from "./components/Navbar/Navbar.js";
+import React from 'react';
+// import Navbar from './components/Navbar/Navbar.js';
+import Header from './components/Navbar/Header.jsx';
 
 function App() {
   return (
     <>
-     <Navbar />
-      {/* <AllProducts />; */}
+      <Header />
     </>
   );
 }
