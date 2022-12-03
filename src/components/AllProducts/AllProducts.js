d7796fb36479095e202db72cf437a57041197d48
OGEmanuel <1blacksanta416@gmail.com>
2022-12-03 01:53:42 +0100

    Pagination added

--- src/components/AllProducts/AllProducts.js
+++ src/components/AllProducts/AllProducts.js
@@ -1,7 +1,157 @@
 import filter from '../../assets/filter.png';
+import nextId from 'react-id-generator';
+
+import first from '../../assets/redBench.png';
+import second from '../../assets/eggBalloon.png';
+import third from '../../assets/Man.png';
+import fourth from '../../assets/Architecture.png';
 
 import classes from './AllProducts.module.css';
 import Products from './Products';
+
+const ALL_PRODUCTS = {
+  List: [
+    {
+      id: nextId(),
+      image: first,
+      description: 'People',
+      name: 'Red Bench',
+      price: '$3.89',
+    },
+
+    {
+      id: nextId(),
+
+      image: second,
+      description: 'Food',
+      name: 'Egg Balloon',
+      price: '$93.89',
+    },
+
+    {
+      id: nextId(),
+
+      image: third,
+      description: 'People',
+      name: 'Man',
+      price: '$100.00',
+    },
+
+    {
+      id: nextId(),
+
+      image: fourth,
+      description: 'Landmarks',
+      name: 'Architecture',
+      price: '$101.00',
+    },
+
+    {
+      id: nextId(),
+
+      image: fourth,
+      description: 'Landmarks',
+      name: 'Architecture',
+      price: '$101.00',
+    },
+
+    {
+      id: nextId(),
+
+      image: third,
+      description: 'People',
+      name: 'Man',
+      price: '$100.00',
+    },
+
+    {
+      id: nextId(),
+
+      image: second,
+      description: 'Food',
+      name: 'Egg Balloon',
+      price: '$93.89',
+    },
+
+    {
+      id: nextId(),
+      image: first,
+      description: 'People',
+      name: 'Red Bench',
+      price: '$3.89',
+    },
+
+    {
+      id: nextId(),
+      image: first,
+      description: 'People',
+      name: 'Red Bench',
+      price: '$3.89',
+    },
+
+    {
+      id: nextId(),
+
+      image: second,
+      description: 'Food',
+      name: 'Egg Balloon',
+      price: '$93.89',
+    },
+
+    {
+      id: nextId(),
+
+      image: third,
+      description: 'People',
+      name: 'Man',
+      price: '$100.00',
+    },
+
+    {
+      id: nextId(),
+
+      image: fourth,
+      description: 'Landmarks',
+      name: 'Architecture',
+      price: '$101.00',
+    },
+
+    {
+      id: nextId(),
+
+      image: fourth,
+      description: 'Landmarks',
+      name: 'Architecture',
+      price: '$101.00',
+    },
+
+    {
+      id: nextId(),
+
+      image: third,
+      description: 'People',
+      name: 'Man',
+      price: '$100.00',
+    },
+
+    {
+      id: nextId(),
+
+      image: second,
+      description: 'Food',
+      name: 'Egg Balloon',
+      price: '$93.89',
+    },
+
+    {
+      id: nextId(),
+      image: first,
+      description: 'People',
+      name: 'Red Bench',
+      price: '$3.89',
+    },
+  ],
+};
 
 const AllProducts = () => {
   return (
@@ -12,7 +162,7 @@ const AllProducts = () => {
         </p>
         <img src={filter} alt="filter" />
       </div>
-      <Products />
+      <Products products={ALL_PRODUCTS.List} />
     </section>
   );
 };
