13e655d01134c90500c4768b2495355f408e5901
Hikmot-O <hikmotokunola@gmail.com>
2022-12-07 05:39:15 +0100

    router setup

---
+++ src/pages/PageNotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PageNotFound = () => {
+  return (
+    <div>
+        <h1 className="not-found">Page not Found!</h1>
+    </div>
+  )
+}
+
+export default PageNotFound;
