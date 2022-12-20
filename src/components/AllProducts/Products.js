005f3aafcfb49a67aab48c1e84e8bda9102c8637
OGEmanuel <1blacksanta416@gmail.com>
2022-12-20 23:58:24 +0100

    Filter by category working on desktop

--- src/components/AllProducts/Products.js
+++ src/components/AllProducts/Products.js
@@ -25,6 +25,12 @@ const Products = props => {
     indexOfLastProduct
   );
 
+  const handleFilter = filtered => {
+    props.onFilter(filtered);
+  };
+  const handlePriceFilter = filteredPrice => {
+    props.onPriceFilter(filteredPrice);
+  };
   return (
     <Fragment>
       <div className={classes.mobile}>
@@ -43,7 +49,12 @@ const Products = props => {
         </div>
 
         <div className={classes['filter-desktop']}>
-          <Filter setFilter={setFilter} filter={filter} />
+          <Filter
+            setFilter={setFilter}
+            filter={filter}
+            onFilter={handleFilter}
+            onFilterPrice={handlePriceFilter}
+          />
         </div>
 
         <div className={classes.content}>
@@ -57,7 +68,7 @@ const Products = props => {
                 <button className={classes.button}>ADD TO CART</button>
                 <p className={classes.description}>{list.description}</p>
                 <p className={classes.name}>{list.name}</p>
-                <p className={classes.price}>{list.price}</p>
+                <p className={classes.price}>${list.price}</p>
               </div>
             ))}
           </div>
